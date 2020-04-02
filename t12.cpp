280d5d43f9b208f0fe091b0126a9954d15a9eb1b
VENOM-X4 <62980850+VENOM-X4@users.noreply.github.com>
2020-04-02 10:46:06 +0530

    Update t12.cpp

--- t12.cpp
+++ t12.cpp
@@ -3,12 +3,27 @@ int main()
 {
 	char ch;
 	int n,i,j,ptr,c,z,sum=0,avgtime;
-	int atime[10],btime[10],wtime[10],priority[10],pn[10],tatime[10];
+	int at[10],bt[10],wt[10],priority[10],pn[10],tat[10];
 	
 
 	printf("__________________________________\n");
 	printf("Enter the the no. of processes : ");
 	scanf("%03d",&n);
 	printf("__________________________________\n");
-	//system("cls");
+		for (i=0;i<n;i++)
+	{
+
+		printf("\n\nprocess [%03d] \n",i+1);
+		printf("++++++++++++++++++++++++\n");
+		printf("Enter arrival time : ");
+		scanf("%03d ",&at[i]);
+    	printf("++++++++++++++++++++++++\n");    
+        printf("Enter Burst time : ");
+		scanf("%03d ",&bt[i]);
+		printf("+++++++++++++++++++++++++\n");
+		printf("Enter Priority : ");
+		scanf("%03d ",&priority[i]);
+		printf("++++++++++++++++++++++++++\n");
+		pn[i]=i+1;
+	}
 
